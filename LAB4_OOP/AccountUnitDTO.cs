214f5cf0d8b46f10ec2f3803f5217e8b269a39a5
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2025-04-27 17:43:37 +0300

    anotations for AccountUnitDTO and Animal

--- LAB4_OOP/AccountUnitDTO.cs
+++ LAB4_OOP/AccountUnitDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace LAB4_OOP
     public class AccountUnitDTO
     {
         public Animal Animal { get; set; }
+        [Range(typeof(DateTime),"2020-01-01","2025-12-31")]
         public DateTime RecDate { get; set; }
+        [Range(2000,20000)]
         public int Price { get; set; }
     }
 }
--- LAB4_OOP/Animal.cs
+++ LAB4_OOP/Animal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,13 @@ namespace LAB4_OOP
 {
     public class Animal
     {
-        private String name, firstName, country;
+        [RegularExpressionAttribute("[а-ЯїЇєЄіІ]{3,20}\\s[а-ЯїЇєЄіІ]{3,20}")]
+        private String name;
+        [RegularExpressionAttribute("[а-ЯїЇєЄіІ]{3,20}")]
+        private String firstName;
+        [RegularExpressionAttribute("[А-ЯЇЄІ]{1}[а-яїєі]{2,20}")]
+        private String country;
+        [Range(typeof(DateTime), "2000-01-01", "2025-12-31")]
         private DateTime birthDate;
 
         public Animal(string name,string firstName,string country,DateTime birtDate)
